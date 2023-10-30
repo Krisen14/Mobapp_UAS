932797524a33116e39890bcc1cdb4d55c3fd5e15
Krisen14 <yaana1409@gmail.com>
2023-10-30 16:29:29 +0700

    Login Page Update

--- app/src/main/java/com/example/uts/MainActivity.kt
+++ app/src/main/java/com/example/uts/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nameEditText: EditText
     private lateinit var loginButton: Button
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,33 +25,34 @@ class MainActivity : AppCompatActivity() {
         nameEditText = findViewById(R.id.textInput)
         loginButton = findViewById(R.id.btn1)
 
-        // Menambahkan listener untuk EditText
+
+
         nameEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // No action needed
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Mengaktifkan tombol login jika input name tidak kosong
+
                 val isNameEmpty = s.isNullOrBlank()
                 loginButton.isEnabled = !isNameEmpty
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // No action needed
+
             }
         })
 
-        // Menambahkan listener untuk tombol login
+
         loginButton.setOnClickListener {
             val name = nameEditText.text.toString()
             if (name.isNotBlank()) {
-                // Pindah ke halaman Home Activity
+
                 val intent = Intent(this, HomeActivity::class.java)
                 intent.putExtra("NAME", name)
                 startActivity(intent)
             } else {
-                // Menampilkan pesan kesalahan jika input name kosong
+
                 Toast.makeText(this, "Name cannot be empty", Toast.LENGTH_SHORT).show()
             }
         }
